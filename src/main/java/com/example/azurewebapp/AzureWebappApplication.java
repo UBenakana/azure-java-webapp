e66ff7be1b8679b2ac9aae9b836d02ff77fc0ae1
uday <udaykumarreddy27@gmail.com>
2024-07-30 16:32:38 +0530

    Initial commit

---
+++ src/main/java/com/example/azurewebapp/AzureWebappApplication.java
@@ -0,0 +1,12 @@
+package com.example.azurewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureWebappApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/azurewebapp/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.azurewebapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Azure Web App!";
+    }
+}
